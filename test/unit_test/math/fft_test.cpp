6ccc1e20546ca1f9b6d1425ea7d04d3ca08c4f86
cuzperf <cuzperf@outlook.com>
2022-11-26 18:06:10 +0800

    update

---
+++ test/unit_test/math/fft_test.cpp
@@ -0,0 +1,32 @@
+#include "math/fft.h"
+
+#include "test/util.h"
+
+namespace cuzperf {
+
+static constexpr double eps = 1e-6;
+
+TEST(MathTest, fft) {
+  {
+    std::vector<FFT::C> a{1, 2, 3, 4};
+    auto b = a;
+    FFT::dft(b);
+    FFT::idft(b);
+    int n = a.size();
+    for (int i = 0; i < n; ++i) {
+      EXPECT_LE(abs(a[i] - b[i]), eps);
+    }
+  }
+  {
+    std::vector<FFT::C> a{1, 2, 3, 4, FFT::C(1, 0.5), FFT::C(2, 0.3), FFT::C(0, 0.5), FFT::C(2.1, 3.4)};
+    auto b = a;
+    FFT::dft(b);
+    FFT::idft(b);
+    int n = a.size();
+    for (int i = 0; i < n; ++i) {
+      EXPECT_LE(abs(a[i] - b[i]), eps);
+    }
+  }
+}
+
+}  // namespace cuzperf
